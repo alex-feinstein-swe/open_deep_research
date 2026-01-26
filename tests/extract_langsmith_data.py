033d64fb4c0a2d8f204279ecf4145b0d53bdddf4
alex-feinstein-swe <alexanderofeinstein@gmail.com>
2026-01-26 13:10:26 -0700

    fixed extract script

--- tests/extract_langsmith_data.py
+++ tests/extract_langsmith_data.py
@@ -41,7 +41,7 @@ def extract_langsmith_data(project_name, model_name, dataset_name, api_key):
     output_jsonl = [
         {
             "id": examples_dict[run.reference_example_id].metadata["id"],
-            "prompt": run.inputs["inputs"]["messages"][0]["content"],
+            "prompt": run.inputs["inputs"]["input"],
             "article": run.outputs["final_report"],
         } for run in runs
     ]
